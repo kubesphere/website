e086de1cf7d241aadc46994ad75cad08807d1314
Zhengyi Lai <zheng1@kubesphere.io>
2022-12-07 18:54:43 +0800

    Add edge function for go-get
    Signed-off-by: Zhengyi Lai <zheng1@kubesphere.io>

---
+++ netlify/edge-functions/go-get/go-get.js
@@ -0,0 +1,16 @@
+export default async (request, context) => {
+  const url = new URL(request.url)
+
+  // Look for the query parameter, and return if we don't find it
+  if (url.searchParams.get('go-get') == '1') {
+    return new Response(`
+<html>
+  <head>
+    <meta name="go-import" content="${url.host}${url.pathname} git https://github.com/kubesphere${url.pathname}">
+    <meta name="go-source" content="${url.host}${url.pathname} https://github.com/kubesphere${url.pathname} https://github.com/kubesphere${url.pathname}/tree/master/{/dir} https://github.com/kubesphere${url.pathname}/blob/master/{/dir}/{/file}#L{/line}">
+  </head>
+</html>`)
+  }
+
+  return
+}
