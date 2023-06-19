512f80d95cda9f47aa771f3327f888aa3ed58d83
hongming <coder.scala@gmail.com>
2023-06-19 10:47:01 +0800

    update go-get.js

--- .netlify/edge-functions/go-get.js
+++ .netlify/edge-functions/go-get.js
@@ -6,8 +6,8 @@ export default async (request, context) => {
     return new Response(`
 <html>
   <head>
-    <meta name="go-import" content="https://kubesphere.io${url.pathname} git https://github.com/kubesphere${url.pathname}">
-    <meta name="go-source" content="https://kubesphere.io${url.pathname} https://github.com/kubesphere${url.pathname} https://github.com/kubesphere${url.pathname}/tree/master/{/dir} https://github.com/kubesphere${url.pathname}/blob/master/{/dir}/{/file}#L{/line}">
+    <meta name="go-import" content="kubesphere.io${url.pathname} git https://github.com/kubesphere${url.pathname}">
+    <meta name="go-source" content="kubesphere.io${url.pathname} _ https://github.com/kubesphere${url.pathname} https://github.com/kubesphere${url.pathname}/tree/master/{/dir} https://github.com/kubesphere${url.pathname}/blob/master/{/dir}/{/file}#L{/line}">
   </head>
 </html>`)
   }
